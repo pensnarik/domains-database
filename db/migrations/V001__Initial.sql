736420f8ebc204ab68760b24565c3b1139ea5fb6
Andrey Zhidenkov <pensnarik@gmail.com>
2020-02-18 14:10:25 +0100

    Add forgotten "dt" column to "site_task" table

--- db/migrations/V001__Initial.sql
+++ db/migrations/V001__Initial.sql
@@ -327,7 +327,8 @@ create table public.site_task
   task_id     integer not null references task(id),
   site_id     integer not null,
   domain      varchar(255),
-  job         varchar(126)
+  job         varchar(126),
+  dt          timestamptz default now()
 );
 
 alter table only site_task
