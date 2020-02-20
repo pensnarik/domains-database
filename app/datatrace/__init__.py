754b0bf36e5b66ef6b6716c461b4a2a91d86ceab
Andrey Zhidenkov <pensnarik@gmail.com>
2020-02-20 12:37:03 +0100

    Version 0.6.20: Handle ValueError exception while saving task result

--- app/datatrace/__init__.py
+++ app/datatrace/__init__.py
@@ -6,5 +6,5 @@ Todo: leave only 1 option in get_config_filename() function
 """
 import os
 
-__version__ = '0.6.19'
+__version__ = '0.6.20'
 __version_info__ = 'Datatrace Crawler, version %s' % __version__
