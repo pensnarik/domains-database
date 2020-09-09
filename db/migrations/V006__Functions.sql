338ccf24147ddf2ab6174283e1b3ef23400b4306
Andrey Zhidenkov <pensnarik@gmail.com>
2020-09-09 21:39:26 +0300

    Update delete_task_from_queue() function so as it decreases number of dispatched jobs for the session

--- db/migrations/V006__Functions.sql
+++ db/migrations/V006__Functions.sql
@@ -1,4 +1,5 @@
-create function public.update_activity
+create or replace
+function public.update_activity
 (
     asession_id integer,
     asites_processed_delta integer
@@ -15,7 +16,8 @@ $$ language plpgsql
 comment on function public.update_activity(integer, integer)
     is 'Increases sites_processed for the given session by asites_processed_delta';
 
-create function public.delete_task_from_queue
+create or replace
+function public.delete_task_from_queue
 (
     aid integer,
     asession_id integer
@@ -25,6 +27,10 @@ begin
       from public.queue
      where id = aid
        and session_id = asession_id;
+
+    update public.session
+       set jobs_dispatched = jobs_dispatched - 1
+     where id = asession_id;
 end;
 $$ language plpgsql
    security definer;
